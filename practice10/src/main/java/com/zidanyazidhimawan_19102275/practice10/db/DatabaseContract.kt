eb1b3dbfefd5d23d53baaa932f25bed7ca900d67
zidanyazid <zidanyazidhimawan@gmail.com>
2022-06-22 01:47:46 +0700

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	practice6/build.gradle
    #	practice6/src/main/AndroidManifest.xml
    #	practice6/src/main/java/com/zidanyazidhimawan_19102275/practice6/MyData.kt
    #	practice6/src/main/java/com/zidanyazidhimawan_19102275/practice6/adapter/ListMyDataAdapter.kt
    #	practice6/src/main/res/drawable/ic_launcher_background.xml
    #	practice6/src/main/res/layout/activity_main.xml
    #	practice6/src/main/res/layout/item_list.xml
    #	practice6/src/main/res/values/colors.xml
    #	practice6/src/main/res/values/strings.xml

---
+++ practice10/src/main/java/com/zidanyazidhimawan_19102275/practice10/db/DatabaseContract.kt
@@ -0,0 +1,16 @@
+package com.zidanyazidhimawan_19102275.practice10.db
+
+import android.provider.BaseColumns
+
+internal class DatabaseContract {
+    internal class QuoteColumns : BaseColumns {
+        companion object {
+            const val TABLE_QUOTE = "quote"
+            const val _ID = "_id"
+            const val TITLE = "title"
+            const val DESCRIPTION = "description"
+            const val CATEGORY = "category"
+            const val DATE = "date"
+        }
+    }
+}
